851ecbc41e7e2c81517d99e3c9eacd7608eba320
Enzo Afonso <135137959+enzoperesafonso@users.noreply.github.com>
2025-07-29 14:17:26 +0200

    fixed hour angle reversal issue

--- src/schier_mount/test_driver.py
+++ src/schier_mount/test_driver.py
@@ -14,7 +14,7 @@ test_calibration_data = {
     'slew_speed': 5000,
 
     'ha_steps_per_degree': 4495610 / 180,
-    'dec_steps_per_degree': 4535120 / 235,
+    'dec_steps_per_degree': 4535120 / 400,
 
     # number of steps we define as a safety buffer around all encoder hardstops
     'limits_safety_factor': 0.005,
