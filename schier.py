8eeb5d8f12ad0f1a41261989804ccdd61e48d1a9
Enzo Afonso <135137959+enzoperesafonso@users.noreply.github.com>
2025-12-04 12:43:41 +0200

    changed slew speeds to 10 deg/s

--- schier.py
+++ schier.py
@@ -80,7 +80,7 @@ class SchierMount():
 
         async with self._com_lock:
             loop = asyncio.get_running_loop()
-            await loop.run_in_executor(None, self.comm.move_to(ra_enc, dec_enc, 24382 * 10, 24382 * 10))
+            await loop.run_in_executor(None, self.comm.move_to(ra_enc, dec_enc, 24382 * 20, 24382 * 20))
         self.state = MountState.PARKING
 
 
