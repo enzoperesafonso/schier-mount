20cd533204f2f193185e43eb5474303592666ac6
Enzo Afonso <135137959+enzoperesafonso@users.noreply.github.com>
2026-02-06 13:55:43 +0200

    fixed target vs command pos in comm module

--- schier.py
+++ schier.py
@@ -61,9 +61,6 @@ class SchierMount():
             self.logger.error(f"Failed to initialize mount: {e}")
             raise
 
-    async def disconnect_mount(self):
-        pass
-
     async def home_mount(self):
         """
         Initiates the homing sequence for both axes.
@@ -138,10 +135,10 @@ class SchierMount():
             self._move_task = asyncio.current_task()
 
             # Use safe_comm to send the park command
-            await self._safe_comm(self.comm.park_mount)
+            await self._safe_comm(self.comm.home_mount)
 
             self.logger.debug("Parking command sent, waiting for encoders to reach target...")
-            await self._await_encoder_stop(tolerance=100, timeout=120)
+            await self._await_encoder_stop(tolerance=10, timeout=120)
 
             self.state = MountState.PARKED
             self.logger.info("Homing sequence completed successfully.")
@@ -174,7 +171,7 @@ class SchierMount():
             await self._safe_comm(self.comm.standby_mount)
 
             self.logger.debug("Standby command sent, waiting for encoders to reach target...")
-            await self._await_encoder_stop(tolerance=100, timeout=120)
+            await self._await_encoder_stop(tolerance=10, timeout=120)
 
             self.state = MountState.IDLE
             self.logger.info("Mount moved to standby pos.")
@@ -233,7 +230,7 @@ class SchierMount():
             await asyncio.sleep(0.2)
         raise TimeoutError("Mount failed to stop within timeout period.")
 
-    async def _await_mount_at_position(self, timeout=180, tolerance=10):
+    async def _await_mount_at_position(self, timeout=180, tolerance=2):
         """
         Wait until current encoder positions match target positions within tolerance.
 
@@ -254,7 +251,7 @@ class SchierMount():
             if ra_diff <= tolerance and dec_diff <= tolerance:
                 return
 
-            await asyncio.sleep(0.1)
+            await asyncio.sleep(0.2)
 
         raise TimeoutError(f"Mount failed to reach target position within {timeout}s ")
 
@@ -266,25 +263,22 @@ class SchierMount():
     async def _status_loop(self):
         while True:
             try:
-                # Acquire the lock ONCE for the whole group of commands
-                async with self.serial_lock:
-                    # We use to_thread inside the lock so no one else can
-                    # interrupt the serial line until all 4 calls finish.
-                    ra_target, ra_actual = await asyncio.to_thread(self.comm.get_encoder_position, 0)
-                    dec_target, dec_actual = await asyncio.to_thread(self.comm.get_encoder_position, 1)
 
-                    ra_axis_status = await asyncio.to_thread(self.comm.get_axis_status_bits, 0)
-                    dec_axis_status = await asyncio.to_thread(self.comm.get_axis_status_bits, 1)
+                ra_target, ra_actual = await self._safe_comm(self.comm.get_encoder_position, 0)
+                dec_target, dec_actual = await self._safe_comm(self.comm.get_encoder_position, 1)
 
-                    self.current_positions = {
-                        "ra_enc": ra_actual, "ra_target_enc": ra_target,
-                        "dec_enc": dec_actual, "dec_target_enc": dec_target,
-                    }
+                ra_axis_status = await self._safe_comm(self.comm.get_axis_status_bits, 0)
+                dec_axis_status = await self._safe_comm(self.comm.get_axis_status_bits, 1)
 
-                    if ra_axis_status['any_error'] or dec_axis_status['any_error']:
-                        self.state = MountState.FAULT
+                self.current_positions = {
+                    "ra_enc": ra_actual, "ra_target_enc": ra_target,
+                    "dec_enc": dec_actual, "dec_target_enc": dec_target,
+                }
+
+                if ra_axis_status['any_error'] or dec_axis_status['any_error']:
+                    self.state = MountState.FAULT
 
             except Exception as e:
                 self.logger.error(f"Status Loop Error: {e}")
 
-            await asyncio.sleep(0.1)  # Slowed down slightly for stability
+            await asyncio.sleep(0.2)  # 5Hz Polling
