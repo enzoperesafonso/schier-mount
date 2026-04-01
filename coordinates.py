3e1a8817654941030c1fe3fda8ef7d992f62d60b
Enzo Afonso <135137959+enzoperesafonso@users.noreply.github.com>
2026-04-01 15:59:56 +0200

    fixed dec axis offset

--- coordinates.py
+++ coordinates.py
@@ -77,8 +77,8 @@ class MountCoordinates:
         Converts encoder counts back to HA and Dec degrees.
         """
         # Convert encoder counts back to mechanical degrees
-        mech_ha = (ra_enc - self.config.encoder['zeropt_ra']) / self.config.encoder['steps_per_deg_ra'] + 13
-        mech_dec = (dec_enc - self.config.encoder['zeropt_dec']) / self.config.encoder['steps_per_deg_dec'] + 13
+        mech_ha = (ra_enc - self.config.encoder['zeropt_ra']) / self.config.encoder['steps_per_deg_ra']
+        mech_dec = (dec_enc - self.config.encoder['zeropt_dec']) / self.config.encoder['steps_per_deg_dec'] - 13
 
         # Determine if we are in Normal or Below-Pole mode based on mech_dec
         # Center of Dec range (120) is the pole.
