6812fa42b0e00590b71f1885e59e2d4113867dbd
Enzo Afonso <135137959+enzoperesafonso@users.noreply.github.com>
2026-04-06 13:49:17 +0200

    reviewed package setup and fixed home not init correctly in schier.

--- src/schier_mount/__init__.py
+++ src/schier_mount/__init__.py
@@ -2,11 +2,6 @@ from .schier import SchierMount, MountState
 from .configuration import MountConfig
 from .comm import MountComm
 from .coordinates import MountCoordinates
-
-try:
-    from .schier_telescope import SchierTelescope
-except ImportError:
-    # pyobs is not installed, so we can't export SchierTelescope
-    SchierTelescope = None
+from .schier_telescope import SchierTelescope
 
 __all__ = ["SchierMount", "MountState", "MountConfig", "MountComm", "MountCoordinates", "SchierTelescope"]
